28ca0403f9a92cea3b2b9141fbf743b7c5c7d79e
MacrinaAshraf <macrina1997@gmail.com>
2020-04-03 22:15:16 +0200

    working on associations between models
    Co-authored-by: NohaHossam123 <nohahossam38@gmail.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  has_many :order_details
+  has_many :users, through: :order_details
+
+  has_many :user_involved_to_order
+  has_many :users, through: :user_involved_to_order
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :order_details
+  has_many :orders, through: :order_details, dependent: :delete_all
+
+  has_many :user_involved_to_order
+  has_many :orders, through: :user_involved_to_order
+
+  has_and_belongs_to_many :users, class_name: 'Friend', foreign_key: 'users_id'
+
+  has_and_belongs_to_many :friends, class_name: 'Friend', foreign_key: 'friend_id'
+
+  has_many :groups, dependent: :delete_all
+
+
+
 end
