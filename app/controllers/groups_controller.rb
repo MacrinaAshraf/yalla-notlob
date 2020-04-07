346b9ddbd4fda258b713f8c2b239f6bd39322153
noha <nohahossam38@gmail.com>
2020-04-08 00:03:31 +0200

    functions for adding and deleting groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,18 +4,25 @@ class GroupsController < ApplicationController
   # GET /groups
   # GET /groups.json
   def index
-    @groups = Group.all
+    @group = Group.new
+    @groups = Group.all()
   end
 
   # GET /groups/1
   # GET /groups/1.json
   def show
+    # if params[:search].blank?  
+    #   redirect_to( groups_path , alert: "Empty field!") and return  
+    # else  
+      @group = params[:search]
+      @results = Group.all.where("lower(name) LIKE :search", search: @group)  
+    # end
   end
 
   # GET /groups/new
-  def new
-    @group = Group.new
-  end
+  # def new
+  #   @group = Group.new
+  # end
 
   # GET /groups/1/edit
   def edit
@@ -24,31 +31,22 @@ class GroupsController < ApplicationController
   # POST /groups
   # POST /groups.json
   def create
-    @group = Group.new(group_params)
-
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to @group, notice: 'Group was successfully created.' }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
+    @group = Group.create(group_params)
+    redirect_to :groups
   end
 
   # PATCH/PUT /groups/1
   # PATCH/PUT /groups/1.json
   def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+    #   if @group.update(group_params)
+    #     format.html { redirect_to @group, notice: 'Group was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @group }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @group.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /groups/1
@@ -69,6 +67,6 @@ class GroupsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def group_params
-      params.fetch(:group, {})
+      params.require(:group).permit(:name)
     end
 end
