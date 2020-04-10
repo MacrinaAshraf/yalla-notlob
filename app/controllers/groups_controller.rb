81f7bf4645c0d17e53a565718e0c62605395dff9
noha <nohahossam38@gmail.com>
2020-04-11 01:59:21 +0200

    controller function of groups page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,13 @@
 class GroupsController < ApplicationController
-  # before_action :authenticate_user!
-  # before_action :set_group, only: [:show, :edit, :update, :destroy]
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
+  helper_method :current_user
 
   # GET /groups
   # GET /groups.json
-<<<<<<< HEAD
+  def current_user
+    @current_user = User.find_by(email).where(user_id:current_user.id)
+  end
+
   def index
     @group = Group.new
     @groups = Group.all()
@@ -13,23 +16,11 @@ class GroupsController < ApplicationController
   # GET /groups/1
   # GET /groups/1.json
   def show
-    # if params[:search].blank?  
-    #   redirect_to( groups_path , alert: "Empty field!") and return  
-    # else  
-      @group = params[:search]
-      @results = Group.all.where("lower(name) LIKE :search", search: @group)  
-    # end
+    @group = Group.find(params[:id])
+    @current_user = params[:search]
+    @users = User.all.where("lower(email) LIKE :search", search: @current_user)
+    return @group , @group.users.create()
   end
-=======
-  # def index
-  #   @groups = Group.all
-  # end
-
-  # GET /groups/1
-  # GET /groups/1.json
-  # def show
-  # end
->>>>>>> 4ce01486e80e80f06313e641caeebfc2b17b5e98
 
   # GET /groups/new
   # def new
@@ -42,7 +33,6 @@ class GroupsController < ApplicationController
 
   # POST /groups
   # POST /groups.json
-<<<<<<< HEAD
   def create
     @group = Group.create(group_params)
     redirect_to :groups
@@ -61,60 +51,25 @@ class GroupsController < ApplicationController
     #   end
     # end
   end
-=======
-  # def create
-  #   @group = Group.new(group_params)
-
-  #   respond_to do |format|
-  #     if @group.save
-  #       format.html { redirect_to @group, notice: 'Group was successfully created.' }
-  #       format.json { render :show, status: :created, location: @group }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @group.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # PATCH/PUT /groups/1
-  # PATCH/PUT /groups/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @group.update(group_params)
-  #       format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @group }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @group.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
->>>>>>> 4ce01486e80e80f06313e641caeebfc2b17b5e98
 
   # DELETE /groups/1
   # DELETE /groups/1.json
-  # def destroy
-  #   @group.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
+  def destroy
+    @group.destroy
+    respond_to do |format|
+      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
-  # private
+  private
     # Use callbacks to share common setup or constraints between actions.
-    # def set_group
-    #   @group = Group.find(params[:id])
-    # end
+    def set_group
+      @group = Group.find(params[:id])
+    end
 
     # Only allow a list of trusted parameters through.
-<<<<<<< HEAD
     def group_params
       params.require(:group).permit(:name)
     end
-=======
-    # def group_params
-    #   params.fetch(:group, {})
-    # end
->>>>>>> 4ce01486e80e80f06313e641caeebfc2b17b5e98
 end
