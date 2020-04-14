3f7acdeec44291af9999268ae5f09e6d036d5c49
noha <nohahossam38@gmail.com>
2020-04-15 01:17:07 +0200

    fixing in function of controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   # before_action :authenticate_user!
   before_action :set_group, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @group = Group.new
     @group = Group.where(user_id: current_user.id)
@@ -13,20 +13,20 @@ class GroupsController < ApplicationController
     @new_group.name = params[:name]
     @new_group.save
     if @new_group.persisted?
-      flash[:notice] = "Group successfully created"
+      flash[:notice] = ["Group successfully created"]
+    elsif @new_group.nil?
+      flash[:create_error] = ["you must enter group name"]
       redirect_to :groups
     end
   end
 
   def show
     
-
   end
 
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
-    # flash[:notice] = " Group successfully deleted"
     redirect_to :groups
   
   end
