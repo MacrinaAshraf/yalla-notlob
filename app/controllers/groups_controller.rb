184c8608a5678e3b8729c15b665de21a54ed941e
noha <nohahossam38@gmail.com>
2020-04-14 16:12:50 +0200

    adding ui for groups page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,58 +2,27 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
   helper_method :current_user
 
-  # GET /groups
-  # GET /groups.json
   def current_user
-    @current_user = User.find_by(email).where(user_id:current_user.id)
+    @current_user = User.find_by(email).where(user_id: current_user.id)
   end
 
   def index
     @group = Group.new
-    @groups = Group.all()
+    @group = Group.where(user_id: current_user.id)
   end
 
-  # GET /groups/1
-  # GET /groups/1.json
-  def show
-    @group = Group.find(params[:id])
-    @current_user = params[:search]
-    @users = User.all.where("lower(email) LIKE :search", search: @current_user)
-    return @group , @group.users.create()
-  end
-
-  # GET /groups/new
-  # def new
-  #   @group = Group.new
-  # end
-
-  # GET /groups/1/edit
-  # def edit
-  # end
-
-  # POST /groups
-  # POST /groups.json
   def create
-    @group = Group.create(group_params)
+    @group = Group.create(group_params, user_id: current_user.id)
+    flash[:notice] = "Group successfully created"
     redirect_to :groups
   end
 
-  # PATCH/PUT /groups/1
-  # PATCH/PUT /groups/1.json
-  def update
-    # respond_to do |format|
-    #   if @group.update(group_params)
-    #     format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-    #     format.json { render :show, status: :ok, location: @group }
-    #   else
-    #     format.html { render :edit }
-    #     format.json { render json: @group.errors, status: :unprocessable_entity }
-    #   end
-    # end
+  def show
+    @group = Group.find(params[:id])
+    @current_user = params[:search]
+    @users = User.all.where("email LIKE :search", search: @current_user)
   end
 
-  # DELETE /groups/1
-  # DELETE /groups/1.json
   def destroy
     @group.destroy
     respond_to do |format|
@@ -63,12 +32,11 @@ class GroupsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
     def set_group
       @group = Group.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def group_params
       params.require(:group).permit(:name)
     end
