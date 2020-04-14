b0a9f0e76f08b6eb5cceca3425be922ecce1e6a4
noha <nohahossam38@gmail.com>
2020-04-15 01:18:28 +0200

    function adding and delete user

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,18 +1,20 @@
 class GroupUsersController < ApplicationController
     def create
         @user = User.find_by(email: params[:email])
-        @group = Group.find(params[:id])
-        @groupUsers = GroupUser.find_by(user_id: @user.id , group_id: @group.id)
+        @groupUsers = GroupUser.find_by(user_id: @user.id , group_id: params[:group_id])
         if @group.users.exits?(@user.id)
             flash[:friend_error]="friend is already exist in this group"
-            redirect_to :groups
+            redirect_to :group_users
         else
-            @groupUser = GroupUser.create(user_id: @user.id , group_id: @group.id)
+            @groupUsers = GroupUser.create(user_id: @user.id , group_id: params[:group_id])
             flash[:notice] = "user has added successfully in this group"
-            redirect_to :groups
+            redirect_to :group_users
         end
     end
 
     def destroy
+        @groupUser = Group.find(params[:group_id])
+        @groupUser.group.users.destroy(User.find(params[:id]))
+        redirect_to :group_users
     end
 end
